b943ec59caa6e7105eb01ff85e9cd8b5d81031d2
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-07-09 09:09:37 -0400

    Working on tests

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -92,7 +92,38 @@ public class GroceryReporter {
             result += priceString + "\n";
 
         }
-        result += "Errors               seen: " + parser.exceptionCounter + " times\n";
+        result += "Errors         \t \t seen: " + parser.exceptionCounter + " times\n";
+
+
+
+
+
+
+        String actual = "name:    Milk \t\t seen: 6 times\n" +
+                "============= \t \t =============\n" +
+                "Price: \t 3.23\t\t seen: 5 times\n" +
+                "-------------\t\t -------------\n" +
+                "Price: \t 1.23\t\t seen: 1 time\n" +
+                "\n" +
+                "name:   Bread \t\t seen: 6 times\n" +
+                "============= \t \t =============\n" +
+                "Price: \t 1.23\t\t seen: 6 times\n" +
+                "-------------\t\t -------------\n" +
+                "\n" +
+                "name: Cookies \t\t seen: 8 times\n" +
+                "============= \t \t =============\n" +
+                "Price: \t 2.25\t\t seen: 8 times\n" +
+                "-------------\t\t -------------\n" +
+                "\n" +
+                "name:  Apples \t\t seen: 4 times\n" +
+                "============= \t \t =============\n" +
+                "Price: \t 0.25\t\t seen: 2 times\n" +
+                "-------------\t\t -------------\n" +
+                "Price: \t 0.23\t\t seen: 2 times\n" +
+                "\n" +
+                "Errors         \t \t seen: 4 times\n";
+
+
 
 
         return result;
